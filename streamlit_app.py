2ed931476aa86589e1bcede85a30cbc9d6d34821
arehkemper <arehkemper@gmail.com>
2022-10-03 19:57:55 -0500

    Update streamlit_app.py
    Pre-population of filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ streamlit.header('🍌🥭 Build your own Fruit Smoothie 🥝🍇')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 #lets put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
