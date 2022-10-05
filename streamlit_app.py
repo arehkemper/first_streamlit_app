e1d1d019b77f881fde9bc5d7292243b8ef6afae4
arehkemper <arehkemper@gmail.com>
2022-10-05 16:29:00 -0500

    Update streamlit_app.py
    filter data table only to include selected fruits from the filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,15 +15,16 @@ streamlit.header('🍌🥭 Build your own Fruit Smoothie 🥝🍇')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 #lets put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
 #display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 #display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+###streamlit.text(fruityvice_response.json())
 
 #normalize json
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
