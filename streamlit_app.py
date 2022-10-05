dc80a4445de3ef745ebc1b72945d9c10c46489f9
arehkemper <arehkemper@gmail.com>
2022-10-05 16:20:59 -0500

    Update streamlit_app.py
    normalize json
    output normalized json as a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+#normalize json
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+#output normalized json as a table
+streamlit.dataframe(fruityvice_normalized)
