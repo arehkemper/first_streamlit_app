23ee156a4076799524f8ae4097d05787258b14cc
Adele Rehkemper <arehkemper@gmail.com>
2022-10-06 17:39:52 -0500

    Update streamlit_app.py
    shift stop statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,6 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows=get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
-
-streamlit.stop()
   
 #allow users to add fruit to the list
 #new def
@@ -72,3 +70,5 @@ if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function=insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
+
+  streamlit.stop()
